4f3faa65196db8b8ab3b65e203ad2406a4edcb4d
breezeHH <breeze2@gmx.de>
2022-02-09 22:51:31 +0100

    basic css

---
+++ src/Weather.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Hamburg</h1>
+      <ul>
+        <li>Wednesday, 21:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="Cloudy"
+          />
+          <span className="temperature">7</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul className="currentConditions">
+            <li>Precipitation: 6%</li>
+            <li>Humidity: 96%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
