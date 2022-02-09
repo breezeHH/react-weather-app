00eaef880af31bcd9584f85dfb1ca1ed7e960170
breezeHH <breeze2@gmx.de>
2022-02-09 21:05:35 +0100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://de.linkedin.com/in/kathrin-hesse-0b6215129"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Kathrin
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/breezeHH/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://de.linkedin.com/in/kathrin-hesse-0b6215129"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Kathrin
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/breezeHH/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
